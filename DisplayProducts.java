dfc0286e649fc19f19399d0f16d5bf3e5749628e
Narin <155862914+Goddess-Narin@users.noreply.github.com>
2024-06-20 08:35:43 +0700

    Add files via upload

---
+++ DisplayProducts.java
@@ -0,0 +1,58 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+
+public class DisplayProducts {
+
+    // JDBC URL, username, and password of MySQL server
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/Product_DB";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "rin123";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+
+
+            statement = connection.createStatement();
+
+
+            String query = "SELECT * FROM Product";
+            resultSet = statement.executeQuery(query);
+
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double pricePerUnit = resultSet.getDouble("price_per_unit");
+                boolean activeForSell = resultSet.getBoolean("active_for_sell");
+
+                // Display product information in console
+                System.out.printf("ID: %d, Name: %s, Price per Unit: %.2f, Active for Sell: %b%n",
+                        id, name, pricePerUnit, activeForSell);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // 5. Close JDBC objects
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
